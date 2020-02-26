1193b5cb5822db3bbb778742746dcdc07e88a556
Michael Hillcox <michaelhillcox@gmail.com>
2020-02-26 17:27:28 +0000

    Help now Dm's if you're a higher role, commands can now be excluded using .isHigher from the help text

--- app/commands/help.js
+++ app/commands/help.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const {isHigher} = require("../Utils");
 
 module.exports = class Help {
     constructor() {
@@ -7,15 +8,21 @@ module.exports = class Help {
     }
 
     async execute(message, client, bot, commands) {
-        const space = 50;
+        const higher = isHigher(message.member) !== null;
 
-        await message.channel.send(
+        await (higher ? message.member : message.channel).send(
             new Discord.RichEmbed()
                 .setColor('#00a8ff')
                 .setTitle("Command List")
-                .setDescription("A list of commands for our bot")
-                .addField('Commands', commands.map(e => `\`${bot.prefix}${e.command}\` _${e.help}_`))
-                .setFooter("If you encounter any issues with the bot then please contact an owner or moderator")
+                .setDescription("A list of commands for our bot" + (!higher ? '' : 'You have been DM\'d this list as it contains sensitive commands'))
+                .addField('Commands', commands.map(e => {
+                    // Skip commands that use higher unless the user is higher
+                    if( !higher && e.isHigher )
+                        return;
+
+                    return `\`${bot.prefix}${e.command}\` _${e.help}_`
+                }))
+                .setFooter("If you encounter any issues with the bot then please contact an owner or moderator.")
         );
     }
 };
--- app/commands/status.js
+++ app/commands/status.js
@@ -2,9 +2,12 @@ module.exports = class Status {
     constructor() {
         this.command = "status";
         this.help = "Provides detailed information about the bot.";
+
+        // Removes the command from the help list unless the user is management
+        this.isHigher = true;
     }
 
     execute(message, client, bot) {
-
+        console.log(process.env.MANAGEMENT_ROLES)
     }
 };
